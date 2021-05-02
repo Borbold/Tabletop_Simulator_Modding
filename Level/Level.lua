532e9cbd5d331b9378b555696d6aee22937de224
Borbold <55662517+Borbold@users.noreply.github.com>
2021-05-02 10:32:25 +0700

    Update Level.lua

--- Level/Level.lua
+++ Level/Level.lua
@@ -161,10 +161,6 @@ function Round(num, idp)
   return math.ceil(num*(10^idp))/10^idp
 end
 
-function GetCurrentLevel()
-  return currentLVL
-end
-
 function RebuildAssets()
   local root = 'https://raw.githubusercontent.com/RobMayer/TTSLibrary/master/ui/'
   local rootIn = 'https://img2.freepng.ru/20180418/hlw/kisspng-computer-icons-adventure-hotel-luggage-5ad725017cc0f8.903043971524049153511.jpg'
