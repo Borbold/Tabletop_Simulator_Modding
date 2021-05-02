3c78208e2043ae6cc7ebda39862704ab7f5d585d
Borbold <55662517+Borbold@users.noreply.github.com>
2021-05-02 11:08:40 +0700

    Update Info.lua

--- Info/Info.lua
+++ Info/Info.lua
@@ -131,7 +131,7 @@ end
 function CheckPlayer(playerColor, onlyGM)
   local args = {playerColor = playerColor, onlyGM = onlyGM}
   if not levelGUID then SearchLevel() end
-  getObjectFromGUID(levelGUID).call("ChangeMaxHP")
+  getObjectFromGUID(levelGUID).call("ChangeBoundValues")
   if getObjectFromGUID(levelGUID).call("CheckPlayer", args) then return true end
 end
 function SearchLevel()
