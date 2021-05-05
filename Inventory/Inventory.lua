2ebb286a7cb0f58bb7ceab8ce42132c2f8c79332
Borbold <55662517+Borbold@users.noreply.github.com>
2021-05-05 12:05:52 +0700

    Update Inventory.lua

--- Inventory/Inventory.lua
+++ Inventory/Inventory.lua
@@ -51,9 +51,9 @@ function RemoveItem(player, _, id)
     snap_to_grid = true,
   }
   local newObject = spawnObject(spawnParametrs)
-  newObject.setCustomObject({image = tableItems["testID"][3]})
   newObject.setName(tableItems["testID"][1])
   newObject.setDescription(tableItems["testID"][2])
+  newObject.setCustomObject({image = tableItems["testID"][3]})
   tableItems["testID"] = nil
 
   Wait.time(|| self.UI.setAttribute("testID", "icon", ""), 0.01)
