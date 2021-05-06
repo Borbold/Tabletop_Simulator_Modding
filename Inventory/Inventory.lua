33aa3179ddadb5a063147884fb877a8adf51d648
Bornold <55662517+Borbold@users.noreply.github.com>
2021-05-06 17:30:23 +0800

    Update Inventory.lua

--- Inventory/Inventory.lua
+++ Inventory/Inventory.lua
@@ -1,7 +1,8 @@
 ﻿function UpdateSave()
   local dataToSave = {
     ["tableItems"] = tableItems,
-    ["infoGUID"] = infoGUID, ["statusGUID"] = statusGUID, ["skillsGUID"] = skillsGUID
+    ["infoGUID"] = infoGUID, ["statusGUID"] = statusGUID, ["skillsGUID"] = skillsGUID,
+    ["saveXML"] = self.UI.getXmlTable(), ["saveCustomAsset"] = self.UI.getCustomAssets(),
   }
   local savedData = JSON.encode(dataToSave)
   self.script_state = savedData
@@ -20,13 +21,13 @@ function onLoad(savedData)
     }
     enumStatus = {
       реакция = 1,
-      к_брони = 2,
+      ["к брони"] = 2,
       п_урона = 3,
       с_урону = 4,
       с_э_урону = 5,
       с_радиации = 6,
       с_ядам = 7,
-      шанс_крита = 8,
+      ["шанс_крита"] = 8,
     }
     enumSkills = {
       л_оружие = 1,
@@ -50,13 +51,22 @@ function onLoad(savedData)
       а_игры = 19,
     }
     Wait.time(|| Confer(savedData), 0.4)
-    params = {
+    local params = {
       function_owner = self, click_function = "RemoveItem",
-      -- Если не сработает, пиши в label
-      index = "testID", label = "...",
+      label = "testID",
       position = {5.05, 0.2, 15.3}, rotation = {0, 180, 0},
       width = 400, height = 400,
-      font_size = 340, font_color = {1, 1, 1},
+      font_size = 120, font_color = {1, 1, 1},
+      color = {0.5, 0.5, 0.5},
+      tooltip = "[33ff33]This[ffffff] text appears on mouseover.",
+    }
+    self.createButton(params)
+    params = {
+      function_owner = self, click_function = "RemoveItem",
+      label = "testID",
+      position = {3.8, 0.2, 15.3}, rotation = {0, 180, 0},
+      width = 400, height = 400,
+      font_size = 120, font_color = {1, 1, 1},
       color = {0.5, 0.5, 0.5},
       tooltip = "[33ff33]This[ffffff] text appears on mouseover.",
     }
@@ -65,13 +75,15 @@ function onLoad(savedData)
 end
 
 function Confer(savedData)
-  RebuildAssets()
   local loadedData = JSON.decode(savedData or "")
+  RebuildAssets(--[[loadedData.saveCustomAsset or]] {})
   tableItems = loadedData.tableItems or {}
   infoGUID = loadedData.infoGUID
   statusGUID = loadedData.statusGUID
   skillsGUID = loadedData.skillsGUID
-  ChangeUI()
+  if loadedData.saveXML then
+    --Wait.time(|| self.UI.setXmlTable(loadedData.saveXML), 1)
+  end
 end
 
 function onCollisionEnter(info)
@@ -84,13 +96,13 @@ function onCollisionEnter(info)
   table.insert(cusAss, {name = 'testICON', url = newObject.getCustomObject().image})
   self.UI.setCustomAssets(cusAss)
   
-  
   local newName = newObject.getName()
   local newDescription = newObject.getDescription()
   Wait.time(function()
     self.UI.setAttribute("testID", "icon", "testICON")
     self.UI.setAttribute("testID", "iconColor", "#ffffffff")
-    self.UI.setAttribute("testID", "tooltip", newName .. "\n" .. newDescription)
+    self.editButton({index = 0, tooltip = newName .. "\n" .. newDescription})
+    --Wait.time(|| print(self.UI.getXmlTable()[2].children[1].children[2].children[1].children[1].children[1].attributes["icon"]), 0.2)
   end, 0.01)
   local newUrlImage = newObject.getCustomObject().image
   
@@ -99,12 +111,10 @@ function onCollisionEnter(info)
   if findText then
     ChangeDependentVariables(newDescription:sub(findText))
   end
-  UpdateSave()
+  Wait.time(|| UpdateSave(), 0.2)
 end
 function RemoveItem(obj, color, alt_click)
   --if not id or not self.UI.getAttribute(id, "icon") or self.UI.getAttribute(id, "icon") == '' then return end
-
-  print(obj.params.index) print(obj.params.label)
   
   local selfPosition = self.getPosition()
   local spawnParametrs = {
@@ -112,8 +122,7 @@ function RemoveItem(obj, color, alt_click)
     position = {x = selfPosition.x, y = selfPosition.y + 0.1, z = selfPosition.z - 4},
     rotation = {x = 0, y = 180, z = 0},
     scale = {x = 1, y = 1, z = 1},
-    sound = false,
-    snap_to_grid = true,
+    sound = false, snap_to_grid = true,
   }
   local newObject = spawnObject(spawnParametrs)
   newObject.setName(tableItems["testID"][1])
@@ -129,6 +138,7 @@ function RemoveItem(obj, color, alt_click)
   Wait.time(function()
     self.UI.setAttribute("testID", "icon", "")
     self.UI.setAttribute("testID", "iconColor", "#ffffff00")
+    self.editButton({index = 0, tooltip = ""})
   end, 0.01)
   UpdateSave()
 end
@@ -180,10 +190,6 @@ function ChangeDependentVariables(description, remove)
   end
 end
 
-function ChangeUI()
-  UpdateSave()
-end
-
 function CheckPlayer(playerColor, onlyGM)
   levelGUID = levelGUID or SearchDie("Level")
   local args = {playerColor = playerColor, onlyGM = onlyGM}
@@ -197,14 +203,12 @@ function SearchDie(name)
   end
 end
 
-function RebuildAssets()
+function RebuildAssets(assets)
   local backG = 'https://i.imgur.com/WQiHEAd.png'
   local example1 = 'https://cdn.discordapp.com/attachments/800324103848198174/838979483075608596/moto_glasses.png'
   local example2 = 'https://cdn.discordapp.com/attachments/800324103848198174/838979481347031070/red_mask.png'
-  local assets = {
-    {name = 'uiBackGroundW', url = backG},
-    {name = 'example1', url = example1},
-    {name = 'example2', url = example2},
-  }
+  table.insert(assets, {name = 'uiBackGroundW', url = backG})
+  table.insert(assets, {name = 'example1', url = example1})
+  table.insert(assets, {name = 'example2', url = example2})
   self.UI.setCustomAssets(assets)
 end
