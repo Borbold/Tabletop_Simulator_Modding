f38c5fa99ca5197ff4ca47a18adcf647b0557d2c
Borbold <ivan0.111@mail.ru>
2024-01-08 23:40:15 +0700

    add new functional for init

--- Initiative/Initiative.lua
+++ Initiative/Initiative.lua
@@ -14,6 +14,7 @@ function Confer(savedData)
     local loadedData = JSON.decode(savedData or "")
     currentInitiative = 1
     maxInitiative = 20
+    countMember = 5
     countRound = 1
     ChangeUI()
 end
@@ -23,33 +24,51 @@ function NextStep(player)
 end
 function ChangeStep(value)
     currentInitiative = currentInitiative + value
-    if currentInitiative > maxInitiative then
+    if currentInitiative > countMember then
         currentInitiative = 1
         countRound = countRound + 1
     end
     ChangeUI()
 end
 
+function ChangeMember(_, input, _)
+    countMember = tonumber(input)
+    ChangeUI()
+end
+
 function ChangeUI()
-    for i = 1, 20 do
+    for i = 1, maxInitiative do
         if i == currentInitiative then
             self.UI.setAttribute("lamp" .. i, "image", "uiGreenBut")
-        else
+        elseif i <= countMember then
             self.UI.setAttribute("lamp" .. i, "image", "uiRedBut")
+        else
+            self.UI.setAttribute("lamp" .. i, "image", "uiGrayBut")
         end
     end
     self.UI.setAttribute("countRound", "text", "Round:" .. countRound)
+    self.UI.setAttribute("countMember", "text", countMember)
     UpdateSave()
+end
+
+function Reset()
+    currentInitiative = 1
+    maxInitiative = 20
+    countMember = 5
+    countRound = 1
+    ChangeUI()
 end
 
 function RebuildAssets()
     local backG = 'https://cdn.discordapp.com/attachments/800324103848198174/838062391127441428/brosok.png'
     local redBut = 'https://cdn.discordapp.com/attachments/800324103848198174/1193905025894854750/Unikalnaya.png'
     local greenBut = 'https://cdn.discordapp.com/attachments/800324103848198174/1193905026180075610/Unikalnaya2.png'
+    local grayBut = "https://cdn.discordapp.com/attachments/800324103848198174/1193946708128825458/Unikalnaya3.png"
     local assets = {
       {name = 'uiBackGround', url = backG},
       {name = 'uiRedBut', url = redBut},
       {name = 'uiGreenBut', url = greenBut},
+      {name = 'uiGrayBut', url = grayBut},
     }
     self.UI.setCustomAssets(assets)
 end
