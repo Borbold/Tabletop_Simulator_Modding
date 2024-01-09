9c189142e2b59d8d5f36065fd6da5388fb045ff1
Borbold <ivan0.111@mail.ru>
2024-01-09 21:42:15 +0700

    add more functional for initiative

--- Initiative/Initiative.lua
+++ Initiative/Initiative.lua
@@ -28,12 +28,45 @@ function ChangeStep(value)
         currentInitiative = 1
         countRound = countRound + 1
     end
+    MessageStep(
+        self.UI.getAttribute("nameStep" .. currentInitiative, "text"),
+        self.UI.getAttribute("nameStep" .. (currentInitiative + 1 < countMember and (currentInitiative + 1) or 1), "text")
+    )
     ChangeUI()
 end
 
 function ChangeMember(_, input, _)
     countMember = tonumber(input)
     ChangeUI()
+end
+
+function SortInitiative()
+    local locInit = {}
+    for i = 1, countMember do
+        locInit[i] = {
+            self.UI.getAttribute("nameStep" .. i, "text"),
+            tonumber(self.UI.getAttribute("reactionStep" .. i, "text"))
+        }
+    end
+    local j, flag = 1, 0
+    while true do
+        if locInit[j][2] > locInit[j + 1][2] then
+            self.UI.setAttribute("nameStep" .. j, "text", locInit[j + 1][1])
+            self.UI.setAttribute("reactionStep" .. j, "text", locInit[j + 1][2])
+            self.UI.setAttribute("nameStep" .. j + 1, "text", locInit[j][1])
+            self.UI.setAttribute("reactionStep" .. j + 1, "text", locInit[j][2])
+            local lInit = locInit[j]
+            locInit[j] = locInit[j + 1]
+            locInit[j + 1] = lInit
+            flag = 1
+        end
+        j = j + 1
+        if j == countMember then
+            if flag == 0 then break else flag = 0 end
+            j = 1
+        end
+    end
+    Wait.time(|| ChangeStep(0), 0.25)
 end
 
 function ChangeUI()
@@ -49,6 +82,35 @@ function ChangeUI()
     self.UI.setAttribute("countRound", "text", "Round:" .. countRound)
     self.UI.setAttribute("countMember", "text", countMember)
     UpdateSave()
+end
+
+function StepUP(player, _, id)
+    if player.color != "Black" then print("Только GM") return end
+    ChangeStepInitiative(id, -1)
+end
+function StepDown(player, _, id)
+    if player.color != "Black" then print("Только GM") return end
+    ChangeStepInitiative(id, 1)
+end
+function ChangeStepInitiative(id, where)
+    local locInit = {}
+    for i = 1, countMember do
+        locInit[i] = {
+            self.UI.getAttribute("nameStep" .. i, "text"),
+            tonumber(self.UI.getAttribute("reactionStep" .. i, "text"))
+        }
+    end
+    local j = tonumber(id:sub(5))
+    if (j + where < countMember or where != -1) and (j > 1 or where != -1) and (j < countMember or where != 1) then
+        self.UI.setAttribute("nameStep" .. j, "text", locInit[j + where][1])
+        self.UI.setAttribute("reactionStep" .. j, "text", locInit[j + where][2])
+        self.UI.setAttribute("nameStep" .. j + where, "text", locInit[j][1])
+        self.UI.setAttribute("reactionStep" .. j + where, "text", locInit[j][2])
+    end
+end
+
+function MessageStep(name1, name2)
+    broadcastToAll("[948773]Ход переходит [0FFF74]" .. name1 .. " [948773]Следующий [0FFF74]" .. name2)
 end
 
 function Reset()
