371ce57a8f80adda805d9ce83306e8fc6ece8b29
Borbold <ivan0.111@mail.ru>
2024-04-01 14:14:25 +0700

    reset guid in info

--- Info Reputation/Info.lua
+++ Info Reputation/Info.lua
@@ -339,6 +339,7 @@ function RewriteText(_, input, id)
 end
 
 function Reset(player)
+  statusGUID, skillsGUID, lifeGUID = nil, nil, nil
   majorValue = {0, 0, 0, 0, 0, 0, 0}
   baffValue = {0, 0, 0, 0, 0, 0, 0}
   debaffValue = {0, 0, 0, 0, 0, 0, 0}
