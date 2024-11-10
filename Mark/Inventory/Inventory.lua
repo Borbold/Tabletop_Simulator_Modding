73b6dea3be4d277f82719af845c208aeb469a493
Borbold <ivan0.111@mail.ru>
2024-11-10 12:03:35 +0700

    small fix

--- Mark/Inventory/Inventory.lua
+++ Mark/Inventory/Inventory.lua
@@ -226,9 +226,9 @@ function Overshoot(player, alt, id)
     if self.UI.getAttribute(id, "text") == "0" then return end
 
     local current = self.UI.getAttribute(id, "text")
-    self.UI.setAttribute(id, "text", alt == "-1" and current - 1 or current + 1)
+    self.UI.setAttribute(id, "text", alt == "-2" and current + 1 or current - 1)
     self.UI.setAttribute(id, "textColor", self.UI.getAttribute(id, "textColor"))
-    if alt == "-2" then return end
+    if alt == "-2" or alt == "-3" then return end
     local arg = {tTag = self.UI.getAttribute(id, "value"), playerColor = player.color}
     throw.call("Overshoot", arg)
 end
