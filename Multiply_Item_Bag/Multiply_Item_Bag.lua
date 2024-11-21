1206d3faa36d4b37d53e0b4fcb638cb29aac4717
Borbold <ivan0.111@mail.ru>
2024-11-21 12:07:29 +0700

    upgrade multiply

--- Multiply_Item_Bag/Multiply_Item_Bag.lua
+++ Multiply_Item_Bag/Multiply_Item_Bag.lua
@@ -1,9 +1,8 @@
-local getMultiplyObjectJSON = nil
-local dontLoopInfo = 1
-
 function onLoad()
+    getMultiplyObjectJSON, dontLoopInfo = nil, 1
     self.addContextMenuItem("Clear", ClearMultiplyObject)
-    self.addContextMenuItem("Multiply", MultiplyObject)
+    self.addContextMenuItem("Multiply", MultiplyObject, true)
+    self.addContextMenuItem("Ping", Ping)
 end
 
 function onCollisionEnter(info)
@@ -27,21 +26,33 @@ end
 
 function MultiplyObject()
     if getMultiplyObjectJSON then
-        local bag = getObjectFromGUID(self.getGMNotes())
-        if bag == nil then print("It's item nonexistent") return end
-        local count = tonumber(self.getDescription())
-        for i = 1, count do
-            local newObject = spawnObjectJSON({
-                json = getMultiplyObjectJSON,
-                rotate = {0, 0, 0},
-                position = {self.getPosition().x, self.getPosition().y + 1, self.getPosition().z}
-            })
-            local toPosition = {
-                bag.getPosition().x,
-                bag.getPosition().y + 5 + i * newObject.getScale().y * 0.1,
-                bag.getPosition().z
-            }
-            newObject.setPositionSmooth(toPosition)
+        local info = {}
+        self.getGMNotes():gsub("[^\n%.]+", function(str) table.insert(info, str) end)
+        local count = tonumber(info[#info]:gsub("%D", ""), 10)
+        for i = 1, #info - 1 do
+            local bag = getObjectFromGUID(info[i])
+            if bag == nil then print("It's item nonexistent") return end
+            for i = 1, count do
+                local newObject = spawnObjectJSON({
+                    json = getMultiplyObjectJSON,
+                    rotate = {0, 0, 0},
+                    position = {self.getPosition().x, self.getPosition().y + 1, self.getPosition().z}
+                })
+                local toPosition = {
+                    bag.getPosition().x,
+                    bag.getPosition().y + 5 + i * newObject.getScale().y * 0.1,
+                    bag.getPosition().z
+                }
+                newObject.setPositionSmooth(toPosition)
+            end
         end
+    end
+end
+
+function Ping(playerColor)
+    local info = {}
+    self.getGMNotes():gsub("[^\n%.]+", function(str) table.insert(info, str) end)
+    for i = 1, #info - 1 do
+        Player[playerColor].pingTable(getObjectFromGUID(info[i]).getPosition() + Vector(0, 1, 0))
     end
 end
