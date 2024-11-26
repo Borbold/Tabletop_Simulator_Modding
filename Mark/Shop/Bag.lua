41616310c3bd9bb24e83d27a36655a753ab40b47
Borbold <ivan0.111@mail.ru>
2024-11-26 13:36:49 +0700

    change bag

--- Mark/Shop/Bag.lua
+++ Mark/Shop/Bag.lua
@@ -34,7 +34,7 @@ function onLoad(savedData)
     Wait.time(function()
         local findName, locDesc, idObj = false, "", 0
         for w in resText:gmatch("[^\n%.]+") do
-            if(findName and w:find("cost:")) then containObjects[idObj].description = locDesc:sub(1, #locDesc - 1) containObjects[idObj].call("CreateButton", "Buy for "..(tonumber(w:gsub("%D", ""), 10))) findName = false end
+            if(findName and w:find("cost:")) then containObjects[idObj].description = locDesc:sub(1, #locDesc - 1) getObjectFromGUID(containObjects[idObj].guid).call("CreateButton", "Buy for "..(tonumber(w:gsub("%D", ""), 10))) findName = false end
             if(findName) then locDesc = locDesc..w.."\n" end
             for id,obj in ipairs(containObjects) do
                 if(w == obj.name) then idObj = id findName = true break end
