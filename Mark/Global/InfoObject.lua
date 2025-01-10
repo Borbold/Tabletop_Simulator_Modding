4502a472188bb04374660e8d42651f728b07b41a
Borbold <ivan0.111@mail.ru>
2025-01-10 17:01:18 +0700

    changed the approach to solving the problem of buffs loading

--- Mark/Global/InfoObject.lua
+++ Mark/Global/InfoObject.lua
@@ -3,10 +3,20 @@ function onLoad()
     putObjects = {}
     avatar = getObjectFromGUID(self.getGMNotes())
     avatar.setVar("InfoObjectGUID", self.getGUID())
+    timeOut = false
+    Wait.time(function() timeOut = true end, 5)
 end
 
 function onCollisionEnter(info)
     local obj = info.collision_object
+    if(timeOut == false) then
+        Wait.time(|| AddBuff(obj), 5)
+    else
+        AddBuff(obj)
+    end
+end
+
+function AddBuff(obj)
     if(Global.getVar(obj.getGUID()) or obj.getPosition().y < self.getPosition().y) then return end
     Global.setVar(obj.getGUID(), true)
     
@@ -28,19 +38,23 @@ end
 
 function RemoveBuff(guid)
     local obj = getObjectFromGUID(guid)
-    if(not Global.getVar(obj.getGUID()) or obj.getPosition().y < self.getPosition().y) then return end
+    if(not Global.getVar(obj.getGUID())) then return end
     Global.setVar(obj.getGUID(), false)
 
     local locPos = self.positionToLocal(obj.getPosition())
-    if locPos.y > 0 then
-        local guidObj = obj.getGUID()
-        for i = 1, #putObjects do
-            if guidObj == putObjects[i].guid then
-                table.remove(putObjects, i)
-                break
-            end
+    local guidObj = obj.getGUID()
+    for i = 1, #putObjects do
+        if guidObj == putObjects[i].guid then
+            table.remove(putObjects, i)
+            break
         end
-        if(locColors:find(self.getName())) then Global.call("UpdateInformation", #putObjects == 0 and {{color = self.getName()}} or putObjects) end
-        avatar.call("UpdateInformation", putObjects)
     end
+    if(locColors:find(self.getName())) then Global.call("UpdateInformation", #putObjects == 0 and {{color = self.getName()}} or putObjects) end
+    avatar.call("UpdateInformation", putObjects)
+end
+
+function ChangeNumber(_, alt, id)
+    local count = self.UI.getAttribute(id, "text")
+    self.UI.setAttribute(id, "text", alt == "-1" and count + 1 or count - 1)
+    self.UI.setAttribute(id, "textColor", "White")
 end
