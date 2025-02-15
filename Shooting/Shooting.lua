6b5d6e24d257e8817a4df442e5105f61d7d93d49
Borbold <55662517+Borbold@users.noreply.github.com>
2025-02-15 13:18:27 +0700

    Update Shooting.lua

--- Shooting/Shooting.lua
+++ Shooting/Shooting.lua
@@ -3,9 +3,15 @@ function ChangeInputField(_, input, id)
 end
 
 function Shoot(player)
+    if(not self.UI.getAttribute("max", "text") or not self.UI.getAttribute("min", "text") or not self.UI.getAttribute("count", "text")) then return end
     printToAll("{en}Shoot "..player.steam_name.."{ru}Стреляет "..player.steam_name)
     local max, min, count = tonumber(#self.UI.getAttribute("max", "text") > 0 and self.UI.getAttribute("max", "text") or 100), tonumber(#self.UI.getAttribute("min", "text") > 0 and self.UI.getAttribute("min", "text") or 1), tonumber(#self.UI.getAttribute("count", "text") > 0 and self.UI.getAttribute("count", "text") or 1)
+    local totalShoot = 0
     for i = 1, count do
-        printToAll("{ru}Результат: "..math.random(min, max).."{en}Result: "..math.random(min, max))
+        local shoot = math.random(min, max)
+        printToAll(string.format("{ru}Результат %d: %d".."{en}Result %d: %d", i, shoot, i, shoot))
+        totalShoot = totalShoot + shoot
     end
+    printToAll("-------------------")
+    printToAll("{ru}Общий результат: "..totalShoot.."{en}Total result: "..totalShoot)
 end
