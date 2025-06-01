2f25b9d6b7fb567929f7e77fb2b69335f2156dc6
Borbold <ivan0.111@mail.ru>
2025-06-01 14:48:15 +0300

    add change volume trigger effects

--- Audio Volume Changer/AVC.lua
+++ Audio Volume Changer/AVC.lua
@@ -1,4 +1,6 @@
-local audioVolume = 0.1
+function onLoad()
+    audioVolume = 0.1
+end
 function changeVolume(player, rapidly, id)
     audioVolume = tonumber(string.format("%.2f", rapidly))
     for _, audio in pairs(self.getComponents("AudioSource")) do
@@ -6,6 +8,11 @@ function changeVolume(player, rapidly, id)
     end
 end
 function onObjectLoopingEffect()
+    for _, audio in pairs(self.getComponents("AudioSource")) do
+        audio.set("volume", audioVolume)
+    end
+end
+function onObjectTriggerEffect()
     for _, audio in pairs(self.getComponents("AudioSource")) do
         audio.set("volume", audioVolume)
     end
