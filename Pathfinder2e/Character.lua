35d0a8ba5e2d60f33b6b32fc1214e46a2f82b2fe
Ivan <ivan0.111@mail.ru>
2025-09-24 10:26:18 +0700

    fix stupid issue

--- Pathfinder2e/Character.lua
+++ Pathfinder2e/Character.lua
@@ -211,9 +211,9 @@ function resetChar()
     for ii = 1, 18 do
         charSave_table.skills[ii] = {}
         charSave_table.skills[ii].proficient = false
-        charSave_table[i].skills[ii].expert = false
-        charSave_table[i].skills[ii].master = false
-        charSave_table[i].skills[ii].legendary = false
+        charSave_table.skills[ii].expert = false
+        charSave_table.skills[ii].master = false
+        charSave_table.skills[ii].legendary = false
         charSave_table.skills[ii].mod = 0
     end
     charSave_table.attacks = {}
