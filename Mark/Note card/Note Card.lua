2450f4f8759e10ab3f18b2bea45141688105b666
Borbold <ivan0.111@mail.ru>
2026-03-09 22:52:21 +0700

    small bug

--- Mark/Note card/Note Card.lua
+++ Mark/Note card/Note Card.lua
@@ -6,7 +6,6 @@ end
 
 function onLoad(savedData)
     local loadedData = JSON.decode(savedData or "")
-    self.UI.setAttribute("inputText", "text", self.getGMNotes())
     if(loadedData) then
         Wait.time(function()
             pen = self.UI.getCustomAssets()[2].url
@@ -14,13 +13,14 @@ function onLoad(savedData)
             self.UI.setAttribute("pen", "image", (loadedData.flag == "true") and pen or penCross)
             self.UI.setAttribute("inputText", "interactable", loadedData.flag)
             self.UI.setAttribute("inputText", "raycastTarget", loadedData.flag)
-            self.UI.setAttribute("textZone", "height", calculateHeightString(self.getGMNotes()))
+            ChangeText(nil, self.getGMNotes())
         end, 0.75)
     end
 end
 
 function ChangeText(_, input)
     self.UI.setAttribute("textZone", "height", calculateHeightString(input))
+    self.UI.setAttribute("inputText", "text", input)
     self.setGMNotes(input)
 end
 
