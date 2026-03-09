202f74e44c3d4bdf9ac647684e7c19faad2f8e29
Borbold <ivan0.111@mail.ru>
2026-03-09 22:26:57 +0700

    update notepad

--- Mark/Note card/Note Card.lua
+++ Mark/Note card/Note Card.lua
@@ -1,18 +1,27 @@
+local function calculateHeightString(string)
+    local lenghtString = #string
+    if(lenghtString <= 190) then lenghtString = 190 end
+    return 26*(lenghtString/34)
+end
+
 function onLoad(savedData)
     local loadedData = JSON.decode(savedData or "")
-    self.UI.setAttribute("inputText", "text", self.getDescription())
+    self.UI.setAttribute("inputText", "text", self.getGMNotes())
     if(loadedData) then
         Wait.time(function()
             pen = self.UI.getCustomAssets()[2].url
             penCross = self.UI.getCustomAssets()[3].url
             self.UI.setAttribute("pen", "image", (loadedData.flag == "true") and pen or penCross)
             self.UI.setAttribute("inputText", "interactable", loadedData.flag)
+            self.UI.setAttribute("inputText", "raycastTarget", loadedData.flag)
+            self.UI.setAttribute("textZone", "height", calculateHeightString(self.getGMNotes()))
         end, 0.75)
     end
 end
 
 function ChangeText(_, input)
-    self.setDescription(input)
+    self.UI.setAttribute("textZone", "height", calculateHeightString(input))
+    self.setGMNotes(input)
 end
 
 function ChangeInteract()
@@ -20,5 +29,6 @@ function ChangeInteract()
     if flag == "true" then flag = "false" else flag = "true" end
     self.UI.setAttribute("pen", "image", (flag == "true") and pen or penCross)
     self.UI.setAttribute("inputText", "interactable", flag)
+    self.UI.setAttribute("inputText", "raycastTarget", flag)
     self.script_state = JSON.encode({["flag"] = flag})
 end
